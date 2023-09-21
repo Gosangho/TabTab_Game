7b682849f93459bc1a395146bc024908aa5011b3
Gosangho <noxxxxti@gmail.com>
2023-09-21 16:15:45 +0900

    마우스 클릭시 버튼의 이미지변경
    마우스 클릭시 버튼의 이미지변경

---
+++ Assets/TabTabs/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Button myButton; // 버튼 오브젝트를 연결할 public 변수
+
+    public Sprite normalImage; // 원래 이미지
+    public Sprite pressedImage; // 눌렸을 때 보여질 이미지
+
+    private Image buttonImage;
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        buttonImage = myButton.GetComponent<Image>();
+        // 원래 이미지로 초기화
+        buttonImage.sprite = normalImage;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // 눌렸을 때 보여질 이미지로 변경
+        buttonImage.sprite = pressedImage;
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // 마우스를 땠을 때 원래 이미지로 변경
+        buttonImage.sprite = normalImage;
+        isPressed = false;
+    }
+
+}
